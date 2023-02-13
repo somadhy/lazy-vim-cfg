10feb5904499568f19e133e350e1aa70ee24c168
Denis <somadhy@hotmail.com>
2023-02-13 10:19:16 +0300

    set pwsh as shell in windows_nt

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,6 +5,14 @@ return {
       { "<f7>", "<cmd>ToggleTerm<cr>", mode = { "i", "n", "v", "t" }, desc = "Toggle Terminal" },
     },
     config = function()
+      if vim.loop.os_uname().sysname == "Windows_NT" then
+        vim.cmd([[
+                  set shell=pwsh
+                  set shellcmdflag=-command
+                  set shellquote=\"
+                  set shellxquote=
+                ]])
+      end
       require("toggleterm").setup()
     end,
   },
